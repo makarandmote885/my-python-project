b7d0bb871b965d3ae05ed1d8f06862571d3a3734
Makarand <makarandmote885@gmail.com>
2026-08-26 23:29:10 +0530

    pattern change

---
+++ intro function.py
@@ -0,0 +1,30 @@
+# user-defined function
+def greetiing_someone(name):
+    print(f"hello word {name}")
+    print("Makarand")
+
+#calling function
+greetiing_someone("roy")
+
+
+def even_odd(number):
+    if number % 2==0 :
+        print("even")
+
+    else:
+        print("odd")
+even_odd(10)
+even_odd(97)    
+
+
+# adding
+def add(num1,num2):
+    result = num1 -  num2
+    print(result)
+
+add(97,74)
+add(8393,9298)
+
+
+
+            
